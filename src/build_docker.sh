ae9400504c5dfc7793881d293a9ddb1b1929d266
MOHAPY24 <mohammedmahmoud123b@gmail.com>
2025-11-29 10:33:03 +0400

    fixed build

--- src/build_docker.sh
+++ src/build_docker.sh
@@ -4,6 +4,6 @@ echo "Building Docker image for $1..."
 
 sudo docker build -f src/linsubsys/"$1"/Dockerfile -t "$1" .
 sudo docker run -it "$1" /bin/sh &
-pkill docker
+sleep 1
 sudo chmod -R a+rw src/linsubsys
 exit 0
